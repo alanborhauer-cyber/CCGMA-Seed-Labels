968bc69d89180d55494d80f4e2b249f620146d5a
alanborhauer-cyber <alanborhauer@virginiaso.org>
2026-06-11 17:55:20 -0700

    Fix formatting of page icon in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ import streamlit as st
 # -------------------------------------------------------------
 st.set_page_config(
     page_title="CCMGA Seed Library",
-    _page_icon = "🌵"
+    _page_icon = "🌵",
     layout="centered",
     initial_sidebar_state="collapsed",
 )
