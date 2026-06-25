625b5d7011fca8486560b781cce26b08a4576e3f
alanborhauer-cyber <alanborhauer@virginiaso.org>
2026-06-25 07:27:41 -0700

    Remove Python version information from streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-Python 3.14.5 (v3.14.5:5607950ef23, May 10 2026, 07:38:09) [Clang 21.0.0 (clang-2100.0.123.102)] on darwin
-Enter "help" below or click "Help" above for more information.
 >>> #!/usr/bin/env python3
 ... """
 ... Cochise County Master Gardener Association -- Seed Library
